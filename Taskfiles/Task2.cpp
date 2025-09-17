0879f8bfcc37fb2abc5fbb5edaef196bf0d63272
CODING-DARSH <darshvit20@gmail.com>
2025-09-17 20:28:51 +0530

    Taskfiles

--- Taskfiles/Task2.cpp
+++ Taskfiles/Task2.cpp
@@ -1,16 +1,16 @@
 // Check Palindrome (using front() and back())
-// #include<iostream>
-// #include<string>
-// using namespace std;
+#include<iostream>
+#include<string>
+using namespace std;
 
-// int main(){
-//     string pal="madam";
-//     int i,n=pal.size();
-//     for ( i = 0; i < n; i++)
-//     {
-//         if(pal.front(i)==pal.back(n-i-1)){
+int main(){
+    string pal="madam";
+    int i,n=pal.size();
+    for ( i = 0; i < n; i++)
+    {
+        if(pal.front(i)==pal.back(n-i-1)){
 
-//         }
-//     }
+        }
+    }
     
-// }
+}
--- Taskfiles/Task3.cpp
+++ Taskfiles/Task3.cpp
@@ -5,8 +5,13 @@ using namespace std;
 int main(){
     string s="Darsh";
     int n=s.size(),ascii;
+    if(n%2==1){
+        cout<<"String has even numbers no middle letter";
+    }
+    else{
         ascii=s.at((n-1)/2);
         ascii-=32;
         s.at((n-1)/2)=ascii;
+    }
     cout<<s;
 }
