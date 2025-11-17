11e02775dc9535fcc10a698e1a18f413ce194bf9
CODING-DARSH <darshvit20@gmail.com>
2025-11-17 19:29:58 +0530

    reverse

--- STL/Algodemo2.cpp
+++ STL/Algodemo2.cpp
@@ -6,26 +6,21 @@
 using namespace std;
 int main(){
     vector<int> v={90,70,30,100,210};
-    sort(v.begin(),v.end());    //for asc order 
-    sort(v.begin(),v.end(),greater<int>()); //for sorting in desc order
+    reverse(v.begin(),v.end());    //for asc order 
+    // reverse(v.begin(),v.end(),greater<int>()); //for sorting in desc order
     for(auto i:v){
         cout<<"\n"<<i;
     }
     vector<string> s={"darsh","harsh","yash"};
-    sort(s.begin(),s.end());
+    reverse(s.begin(),s.end());
     for (auto i:s){
         cout<<"\n"<<i;
     }
     string name="darsh";
-    sort(name.begin(),name.end());
+    reverse(name.begin(),name.end());
     for(auto i:name){
         cout<<"\n"<<i;
         }
-      int arr[] = {10,22,30,40,1};
-    sort(arr,arr+5);
-    for(auto x:arr){
-        cout<<"\n"<<x;
-    }
 
 }
 
