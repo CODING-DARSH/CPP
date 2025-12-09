917a6afe85479a14ea3abde9d54f100acba3ebb4
CODING-DARSH <darshvit20@gmail.com>
2025-12-09 21:00:38 +0530

    Queue Syntax implement

---
+++ STL/Queuedemo1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+#include<queue>
+
+using namespace std;
+
+int main(){
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+    cout<<"Front element--->"<<q.front();
+    q.pop();
+    cout<<"Front element--->"<<q.front();
+    cout<<"\n\n";
+    while (!q.empty())
+    {
+        cout<<q.front();
+        q.pop();
+    }
+    
+}
